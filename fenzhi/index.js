9e590be1a29c6ca66bc0ececa3fb913c6b119fe5
fanfei <chris.aimee.2012@gmail.com>
2017-12-29 17:22:58 +0800

    update

--- fenzhi/index.js
+++ fenzhi/index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './src/sample10/FlatListSample';
+import App from './src/sample11/SwipoutSample';
 
 AppRegistry.registerComponent('fenzhi', () => App);
---
+++ fenzhi/src/sample11/SwipoutSample.js
@@ -0,0 +1,121 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  Image,
+  Keyboard,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+  Dimensions,
+  FlatList,
+  Alert
+}
+from 'react-native';
+import Swipeout from 'react-native-swipeout';
+
+let data = [
+  {
+      "key":"1",
+      "name":"name1",
+      "description":"description1",
+      "urlAddress":"http://img0.imgtn.bdimg.com/it/u=3646823212,2542355244&fm=27&gp=0.jpg"
+  },
+  {
+      "key":"2",
+      "name":"name2",
+      "description":"description2",
+      "urlAddress":"http://img0.imgtn.bdimg.com/it/u=3646823212,2542355244&fm=27&gp=0.jpg"
+  },
+  {
+      "key":"3",
+      "name":"name3",
+      "description":"description3",
+      "urlAddress":"http://img0.imgtn.bdimg.com/it/u=3646823212,2542355244&fm=27&gp=0.jpg"
+  },
+  {
+      "key":"4",
+      "name":"name4",
+      "description":"description4",
+      "urlAddress":"http://img0.imgtn.bdimg.com/it/u=3646823212,2542355244&fm=27&gp=0.jpg"
+  }
+];
+
+export default class SwipoutSample extends Component {
+
+
+
+
+  render() {
+
+    const swipeSettings = {
+      autoClose:true,
+      onClose:()=>{
+
+      },
+      onOpen:()=>{
+
+      },
+      right:[
+        {
+          onPress: () => {
+            Alert.alert(
+              'Alert',
+              'Are you want to delete this item?',
+              [
+                {text:'NO', onPress:()=>{console.log('no pressed')},style:'cancel'},
+                {text:'YES', onPress:()=>{console.log('yes pressed')}}
+              ],
+              {'cancelable':true}
+
+            );
+          },
+          text:'Delete',
+          type:'delete'
+        }
+      ]
+    };
+
+    return (
+
+      <Swipeout {...swipeSettings}>
+            <View style={{flex :1, marginTop: 20}}>
+              <FlatList
+                data={data}
+                renderItem={(items)=>{
+                  return (
+                    <View
+                      style={{
+                        flex:1,
+                        flexDirection: 'row',
+                        backgroundColor: items.index%2==0?'green':'red'}}>
+
+                          <Image
+                            style={{width: 100,height: 100, marginTop: 5}}
+                            source={require('../sample08/test.jpg')}></Image>
+
+
+                        <View>
+                          <Text style={{padding: 20}}>{items.item.name}</Text>
+                          <Text style={{padding: 20}}>{items.item.description}</Text>
+                        </View>
+                  </View>);
+
+                }}>
+
+              </FlatList>
+            </View>
+      </Swipeout>
+    );
+  }
+
+}
