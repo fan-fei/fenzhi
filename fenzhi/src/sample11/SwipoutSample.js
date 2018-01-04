ad84c3d9383318fc4c9b001fdff14c64222018cb
fanfei <chris.aimee.2012@gmail.com>
2018-01-04 16:30:24 +0800

    update

--- fenzhi/src/sample11/SwipoutSample.js
+++ fenzhi/src/sample11/SwipoutSample.js
@@ -27,103 +27,87 @@ let data = [
   {
       "key":"1",
       "name":"name1",
-      "description":"description1",
-      "urlAddress":"http://img0.imgtn.bdimg.com/it/u=3646823212,2542355244&fm=27&gp=0.jpg"
+      "description":"please swipe to left for deleting 1",
   },
   {
       "key":"2",
       "name":"name2",
-      "description":"description2",
-      "urlAddress":"http://img0.imgtn.bdimg.com/it/u=3646823212,2542355244&fm=27&gp=0.jpg"
+      "description":"please swipe to left for deleting 2",
   },
   {
       "key":"3",
       "name":"name3",
-      "description":"description3",
-      "urlAddress":"http://img0.imgtn.bdimg.com/it/u=3646823212,2542355244&fm=27&gp=0.jpg"
+      "description":"please swipe to left for deleting 3",
   },
 ];
 
 export default class SwipoutSample extends Component {
-
-
-
-
   render() {
-
-
     return (
-            <View style={{flex :1, marginTop: 20}}>
-              <FlatList
-                data={data}
-                renderItem={(items)=>{
+      <View style={{flex :1, marginTop: 20}}>
+        <FlatList
+          data={data}
+          renderItem={(items)=>{
+            const swipeSettings = {
+              autoClose:true,
 
+              onClose:()=>{
+              },
 
-                  const swipeSettings = {
-                    autoClose:true,
+              onOpen:()=>{
+              },
 
-                    onClose:()=>{
-                    },
+              right:[
+                {
+                  onPress: () => {
+                    Alert.alert(
+                      'Alert',
+                      'Are you want to delete this item?',
+                      [
+                        {text:'NO', onPress:()=>{console.log('no pressed')},style:'cancel'},
+                        {text:'YES', onPress:()=>{
+                            data.splice(items.index,1);
+                            this.setState(()=>{return {};});
+                          }
+                        }
+                      ],
+                      {'cancelable':true}
 
-                    onOpen:()=>{
-
-                    },
-
-                    right:[
-                      {
-                        onPress: () => {
-                          Alert.alert(
-                            'Alert',
-                            'Are you want to delete this item?',
-                            [
-                              {text:'NO', onPress:()=>{console.log('no pressed')},style:'cancel'},
-                              {text:'YES', onPress:()=>{
-                                  data.splice(items.index,1);
-                                  // alert(JSON.stringify(data))
-                                }
-                              }
-                            ],
-                            {'cancelable':true}
-
-                          );
-                        },
-                        text:'Delete',
-                        type:'delete'
-                      }
-                    ]
-                  };
+                    );
+                  },
+                  text:'删除',
+                  type:'delete'
+                }
+              ]
+            };
 
 
 
-                  return (
+            return (
+              <Swipeout   {...swipeSettings} >
+                <View
+                  style={{
+                    flex:1,
+                    flexDirection: 'row',
+                    backgroundColor: items.index%2==0?'red':'green'}}>
 
-                    <Swipeout   {...swipeSettings} >
-                    <View
-                      style={{
-                        flex:1,
-                        flexDirection: 'row',
-                        backgroundColor: items.index%2==0?'green':'red'}}>
-
-                          <Image
-                            style={{width: 100,height: 100, marginTop: 5}}
-                            source={require('../sample08/test.jpg')}></Image>
+                  <Image
+                    style={{width: 100,height: 100, marginTop: 5}}
+                    source={require('../sample08/test.jpg')}>
+                  </Image>
 
 
-                        <View>
-                          <Text style={{padding: 10}}>{items.index+1}</Text>
-                          <Text style={{padding: 10}}>{items.item.name}</Text>
-                          <Text style={{padding: 10}}>{items.item.description}</Text></View>
+                  <View>
+                    <Text style={{padding: 10}}>{items.item.key}</Text>
+                    <Text style={{padding: 10}}>{items.item.name}</Text>
+                    <Text style={{padding: 10}}>{items.item.description}</Text></View>
                   </View>
                 </Swipeout>
-                );
-
-
-                }}>
-
-              </FlatList>
-            </View>
-
+            );
+          }}>
+        </FlatList>
+        <TextInput style={{borderWidth: 1, borderColor: "black"}} autoCapitalize="words"></TextInput>
+      </View>
     );
   }
-
 }
